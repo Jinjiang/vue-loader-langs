926a792b7639fca3c8ed46722f2a39e0636b59b1
Jinjiang <zhaojinjiang@me.com>
2018-02-05 23:59:18 +0800

    small tweaks

--- sass/webpack.config.js
+++ sass/webpack.config.js
@@ -14,18 +14,9 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: cssLoaders,
-      },
-      {
-        test: /\.scss$/,
-        use: scssLoaders,
-      },
-      {
-        test: /\.sass$/,
-        use: sassLoaders,
-      },
+      { test: /\.css$/, use: cssLoaders },
+      { test: /\.scss$/, use: scssLoaders },
+      { test: /\.sass$/, use: sassLoaders },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
@@ -34,8 +25,8 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': scssLoaders,
-            'sass': sassLoaders
+            scss: scssLoaders,
+            sass: sassLoaders
           }
           // other vue-loader options go here
         }
