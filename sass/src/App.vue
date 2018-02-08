1a98c49c96cb0010ea4bbc99d05f237a800e2c1b
Jinjiang <zhaojinjiang@me.com>
2018-02-08 22:14:16 +0800

    small tweak

--- sass/src/App.vue
+++ sass/src/App.vue
@@ -38,13 +38,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  h1, h2 {
+    font-weight: normal;
+  }
 }
 </style>
 
 <style lang="sass">
-h1, h2
-  font-weight: normal;
-
 ul
   list-style-type: none;
   padding: 0;
